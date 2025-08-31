c1536da90cd139b7d513405c70c193f464a7ad2a
Igor Grosu <64898387+Igor416@users.noreply.github.com>
2025-08-31 22:17:49 +0300

    created modules and topics

--- api/models/manual/with_slug.py
+++ api/models/manual/with_slug.py
@@ -4,7 +4,7 @@ from uuid import uuid4
 class WithSlug(models.Model):
   id = models.UUIDField('id', default=uuid4, primary_key=True)
   name = models.CharField('Название', max_length=192)
-  slug = models.SlugField('Слаг', max_length=64, db_index=True)
+  slug = models.SlugField('Слаг', max_length=128, db_index=True)
   order = models.SmallIntegerField('Порядок', default=0)
   
   def __str__(self):
